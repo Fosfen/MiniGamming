a281c229e45bd37a11faaba6d6454a4197500605
DELHALLE Sébastien <delhallesebastien@gmail.com>
2018-10-12 11:44:45 +0200

    Classe Mots

--- src/Model/Mots.java
+++ src/Model/Mots.java
@@ -1,6 +1,24 @@
 package Model;
 
+import java.io.File;
+
 public abstract class Mots extends AbstractModel
 {
+    protected String reponse; // Mot à trouver
+    protected String choix; // Choix du joueur
 
+    // Constructeur
+    public Mots ()
+    {
+        // Constructeur de AbstractModel
+        super();
+
+        this.choix = "";
+        this.reponse = genererMot(new File("fileName")); // Géneration du mot à trouver à partir d'un fichier //TODO
+    }
+
+    private String genererMot(File dictionnaire)
+    {
+        return ""; // TODO
+    }
 }
