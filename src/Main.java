a1c37e121799d2c361eb328e262a0eabdb842403
DELHALLE Sébastien <delhallesebastien@gmail.com>
2018-11-06 16:49:32 +0100

    Génération de mot reponse dans les jeux à mots

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import Model.Mots;
+import Model.Pendu;
 import View.MenuFrame;
 
-public class Main {
-
-    public static void main(String[] args) {
-        MenuFrame menu = new MenuFrame();
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //MenuFrame menu = new MenuFrame();
+        Pendu pendu = new Pendu();
+        System.out.println(pendu.getReponse());
     }
-
 }
--- src/Model/Mots.java
+++ src/Model/Mots.java
@@ -1,11 +1,15 @@
 package Model;
 
-import java.io.File;
+import java.io.*;
+import java.util.Random;
 
 public abstract class Mots extends AbstractModel
 {
     protected String reponse; // Mot à trouver
     protected String choix; // Choix du joueur
+
+    // target : valeur correspondant à la ligne du dictionnaire qui sera choisie
+    private final int target = new Random().nextInt(369086)+1;
 
     // Constructeur
     public Mots ()
@@ -14,11 +18,43 @@ public abstract class Mots extends AbstractModel
         super();
 
         this.choix = "";
-        this.reponse = genererMot(new File("fileName")); // Géneration du mot à trouver à partir d'un fichier //TODO
+        this.reponse = genererMot(new File("src/resCode/dictionnaire.txt")); // Géneration du mot à trouver à partir d'un fichier //TODO
     }
 
     private String genererMot(File dictionnaire)
     {
-        return ""; // TODO
+        try
+        {
+            FileReader fr = new FileReader(dictionnaire);
+            BufferedReader br = new BufferedReader(fr);
+
+            try
+            {
+                int i = 1;
+                while (i < target)
+                {
+                    br.readLine();
+                    i++;
+                }
+
+                String motTarget = br.readLine();
+                return motTarget;
+            }
+            catch (IOException exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+        catch (FileNotFoundException exception)
+        {
+            System.out.println("Le fichier n'a pas été trouvé...");
+        }
+
+        return "";
+    }
+
+    public String getReponse()
+    {
+        return this.reponse;
     }
 }
