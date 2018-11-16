79ae4a46bb419fa5205e733d94da238234e19ff4
Raphael <d.rapha.rd@gmail.com>
2018-11-16 11:50:29 +0100

    Create files

---
+++ src/View/JeuView/JSudokuLettre.java
@@ -0,0 +1,21 @@
+package View.JeuView;
+
+import Controller.AbstractController;
+
+public class JSudokuLettre extends JeuView {
+    public JSudokuLettre(AbstractController controller) {
+        super(controller);
+    }
+
+    @Override
+    public void update()
+    {
+
+    }
+
+    @Override
+    public void update(String field) {
+
+    }
+}
+
